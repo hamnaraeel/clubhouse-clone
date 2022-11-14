367eb142803da0ba34f1ede656ed085cb1c7d851
Hamna R <hamnaraeel@gmail.com>
2022-11-14 21:19:59 +0500

    ADDED: PhoneInput continue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import PlanLayout from './pages/Layouts/PlanLayout';
 import Welcome from './pages/Welcome';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PhoneConfirmation from './pages/PhoneConfirmation';
 
 function App() {
   return (
     <BrowserRouter>
       <PlanLayout>
-        <Welcome />
+        <Routes>
+        <Route exact path='/' element={ <Welcome /> } />
+        <Route exact path='/invite' element={<PhoneConfirmation /> } />
+        </Routes>
       </PlanLayout>
     </BrowserRouter>
     );
