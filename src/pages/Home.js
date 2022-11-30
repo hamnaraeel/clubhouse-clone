5811d2d66a5af84326095991acfa89e60c56d0cd
Hamna R <hamnaraeel@gmail.com>
2022-11-30 12:54:31 +0500

    Adding explore into the app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { BsGrid3X3Gap } from "react-icons/bs";
 import BottomSheet from "../components/BottomSheet";
 import data from "../data/roomCard.json"
+import newRoomData from "../data/newRoomData.json"
 
 export default function Home() {
     const [itemsVisible, setItemsVisible] = useState(true);
@@ -60,6 +61,13 @@ export default function Home() {
                         setLoaderVisibility(false)
                     }, 1000);
                 }}
+            />
+            <BottomSheet
+                sheetTitle = "New room"
+                setSheetVisible={(item) =>  setSheetCreateRoom(item)}
+                sheetVisible={sheetCreateRoom}
+                cardDetail={newRoomData}
+                setItemsVisible={(item) => setItemsVisible(item)}
             />
         </>
     );
